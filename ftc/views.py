3cd1a1bc6a17c7971aa8a1d12e0bc8108aa6744d
rupeshkanth218 <75009562+rupeshkanth218@users.noreply.github.com>
2022-11-04 12:03:26 +0530

    static files fixed sort of

--- ftc/views.py
+++ ftc/views.py
@@ -3,9 +3,11 @@ from django.http import FileResponse, Http404
 from django.templatetags.static import static
 # Create your views here.
 def home(request):
-    try:
-        url=static('Readme.pdf')
-        return FileResponse(open(url,'rb'),content_type="application/pdf")
-    except FileNotFoundError:
-        raise Http404()
+    # try:
+    url="ftc/"+static('Readme.pdf')
+    
+    return FileResponse(open(url,'rb'),content_type="application/pdf")
+    # except FileNotFoundError:
+        
+    #     raise Http404()
 
